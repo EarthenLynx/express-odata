6a39577f753a53a6060d7b232cbc45994356721a
Tobias Quante <quan@ads.hettich.com>
2020-06-15 12:05:39 +0200

    Added the create route functionality

--- controller/admin/routes/createRoute.js
+++ controller/admin/routes/createRoute.js
@@ -1,5 +1,19 @@
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const adapter = new FileSync("routes_db.json");
+const routes_db = low(adapter);
+
 const CREATE_ROUTE = (req, res, next) => {
-    console.warn("Create Route has been hit");
-}
+  // Create the DB file if it doesn't exist yet
+  console.log("This create route");
+  
+  routes_db.defaults({ routes: [] }).write();
+
+  routes_db.get("routes").push(req.body).write();
+  res.send({"msg": "received request"})
+  // Add a post
+//   db.get("routes").push({ id: 1, title: "lowdb is awesome" }).write();
+};
 
 module.exports = CREATE_ROUTE;
