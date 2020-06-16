1f45422c09acb5617c1dffff2b01d9e4d6b49d46
Tobias Quante <quan@ads.hettich.com>
2020-06-16 13:43:23 +0200

    Cleanup of old files. Added auth header in axios

--- config/axios.config.js
+++ config/axios.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   headers: {
-    // Authorization:
-    //   "Basic " + Buffer.from(username + ":" + password).toString("base64"),
+    Authorization:
+      "Basic " +
+      Buffer.from("user" + ":" + "passwort").toString("base64"),
     Accept: "application/json",
     "Content-Type": "application/json",
   },
